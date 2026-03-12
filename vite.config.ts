0d662d19e9cff68a3cb7ac8777ca979b6697c0fa
NguyenDaiQuoc <luutrithon1996@gmail.com>
2026-03-12 10:59:23 +0700

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "/NguyenDaiQuoc/Profile",
+    base: "/Profile/",
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
